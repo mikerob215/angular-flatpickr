15fbeae7a9c3e32acec1b65c19e8f99b6fda610f
mikerobinson <mikerobinson1@gmail.com>
2017-01-07 13:14:34 -0500

    reduces package to just the angular module, ran build

--- dist/ng-flatpickr.js
+++ dist/ng-flatpickr.js
@@ -1,36 +1,26 @@
-(function(root, factory) {
-  'use strict';
-  root['angular-flatpickr'] = factory(root.angular, root.flatpickr);
-}(this, function(angular, flatpickr) {
-
-  'use strict';
-  var ngFlatpickr = angular.module('angular-flatpickr', []);
-  ngFlatpickr.directive('ngFlatpickr', [function() {
+angular.module('angular-flatpcikr', [])
+    .directive('ngFlatpickr', [function() {
     return {
-      require: 'ngModel',
-      restrict : 'A',
-      scope : {
-        fpOpts : '&',
-        fpOnSetup : '&'
-      },
-      link : function(scope, element, attrs, ngModel) {
+        require: 'ngModel',
+        restrict : 'A',
+        scope : {
+            fpOpts : '&',
+            fpOnSetup : '&'
+        },
+        link : function(scope, element, attrs, ngModel) {
 
-        var vp = new Flatpickr(element[0], scope.fpOpts());
+            var vp = new Flatpickr(element[0], scope.fpOpts());
 
-        if (scope.fpOnSetup) {
-          scope.fpOnSetup({
-            fpItem : vp
-          });
+            if (scope.fpOnSetup) {
+                scope.fpOnSetup({
+                    fpItem : vp
+                });
+            }
+            element.on('click', function (e) {
+                scope.$apply(function() {
+                    ngModel.$setViewValue(vp.selectedDateObj);
+                });
+            });
         }
-        element.on('click', function (e) {
-          scope.$apply(function() {
-            ngModel.$setViewValue(vp.selectedDateObj);
-          });
-        });
-      }
     };
-  }]);
-
-  return ngFlatpickr;
-
-}));
+}]);
